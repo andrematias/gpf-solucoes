3270cf83c4fa5ff59f856fe6a66ee4b76f3702fd
André Matias <andredevelopers@outlook.com>
2015-04-20 12:20:36 -0300

    Adicionando comentarios para organizar os códigos

--- index.php
+++ index.php
@@ -7,12 +7,19 @@
 		<title>GPF Soluções</title>
 	</head>
 	<body>
+		<!--DIV CONTEINER PARA "ENCAIXOTAR O SITE"-->
 		<div class= "conteiner">
+			<!--CABEÇALHO DO SITE-->
 			<div class = "header">
 				<img src="imagens/logo.jpg" alt= "GPF Soluções" title = "GPF Soluções"/>
 			</div>
+			<!--FIM DO CABEÇALHO DO SITE-->
 
+
+			<!--SLIDE CLASS REGIÃO DO SLIDE BACKGROUND-->
 			<div class = "slideConteiner">
+			<!--SLIDE BOX REGIÃO DO SLIDE COM CAIXA DA ESQUERDA, CENTRAL E DIREITA PARA BACKGROUND INTEGRADO COM O MENU, SLIDE PROPRIO E BACKGRAUND DIREITO-->
+
 				<div id = "slideBox">
 					<div id = "sLeft">
 
@@ -27,18 +34,24 @@
 					</div>
 				</div>
 			</div>
-
+			<!--FIM DO SLIDE CLASS REGIÃO DO SLIDE-->
+			<!--CLASS MENUBAR ARMAZENA LINKS DO MENU E BACKGROUND-->
 			<div class = "menuBar">
 
 			</div>
-
+			<!--FIM DA CLASS MENUBAR ARMAZENA LINKS DO MENU E BACKGROUND-->
+			<!--CORPO DO CONTEÚDO DO SITE-->
 			<div class = "corpo">
 
 			</div>
-
+			<!--CORPO DO CONTEÚDO DO SITE-->
+			<!--RODAPÉ CONTEM OS LINKS E DIREITOS AUTORAIS-->
 			<div class = "rodape">
 
 			</div>
+			<!--FIM DO RODAPÉ CONTEM OS LINKS E DIREITOS AUTORAIS-->
+		<!--FIM DA DIV CONTEINER PARA "ENCAIXOTAR O SITE"-->
 		</div>
+
 	</body>
 </html>
