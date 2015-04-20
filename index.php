2f646ec400173f99fb0f27401af100d2c3fe2af0
André Matias <andredevelopers@outlook.com>
2015-04-20 18:29:07 -0300

    Adicionando div conteudo, paragrafos e titulo dentro da div conteudo

--- index.php
+++ index.php
@@ -49,7 +49,20 @@
 			<!--FIM DA CLASS MENUBAR ARMAZENA LINKS DO MENU E BACKGROUND-->
 			<!--CORPO DO CONTEÚDO DO SITE-->
 			<div class = "corpo">
+				<!--CONTEÚDO DO SITE-->
+				<div id = "conteudo">
+					<!--Titulo do texto-->
+					<h1 class = "titulos"> Um pouco sobre nós. </h1>
+					<p class = "paragrafos">
 
+						A empresa foi idealizada por dois eletricista especialisados em " ... ", Paulo e Freitas que naquele ano trabalhavam em um heliporto na cidade de Carapicuíba - SP, onde eram responsáveis pela manutenção e concervação da rede elétrica do local. </br>
+						Ao decorrer dos anos foram aprimorando seus conhecimentos investindo em cursos qualificativos e adquirindo experiencias nas áres que atuavam em seu ambiente de trabalho, para oferecer serviços com qualidades garantidas ao clientes.</br>
+						No Ano de "1999" decidiram inaugurar a GPF Soluções, nome o qual é a junção dos nomes dos proprietários (Grupo Paulo e Freitas Soluções), inicialmente atuavam somente no ramo de elétrica, focada em "...", após uma pesquisa de mercado e analisando a demanda de seus cliente investiram em qualificações para trabalharem com hidráulica, redes estruturada, PABX, CFTV, instalações de Ar Condicionados, Câmeras de Segurança e instalações de grupo de Gerador com a mesma eficiencia.</br>
+						A GPF Soluções atende em todas as regiões de São Paulo, sua sede esta localizada em Carapicuíba - SP e conta com uma equipe de "..." funcionários treinados para exercer as atividades.
+
+					</p>
+
+				</div>
 			</div>
 			<!--CORPO DO CONTEÚDO DO SITE-->
 			<!--RODAPÉ CONTEM OS LINKS E DIREITOS AUTORAIS-->
