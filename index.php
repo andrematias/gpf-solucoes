f68f8f68062997ad0613c6c9f978794251923b0d
André Matias <andredevelopers@outlook.com>
2015-04-20 12:01:36 -0300

    Criação de wireframe do website, delineando as bordas das divs

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang = "pt-br">
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<meta charset = "UTF-8">
+		<title>GPF Soluções</title>
+	</head>
+	<body>
+		<div class= "conteiner">
+			<div class = "header">
+				<img src="imagens/logo.jpg" alt= "GPF Soluções" title = "GPF Soluções"/>
+			</div>
+
+			<div class = "slideConteiner">
+				<div id = "slideBox">
+					<div id = "sLeft">
+
+					</div>
+
+					<div id = "slide">
+
+					</div>
+
+					<div id = "sRigth">
+
+					</div>
+				</div>
+			</div>
+
+			<div class = "menuBar">
+
+			</div>
+
+			<div class = "corpo">
+
+			</div>
+
+			<div class = "rodape">
+
+			</div>
+		</div>
+	</body>
+</html>
